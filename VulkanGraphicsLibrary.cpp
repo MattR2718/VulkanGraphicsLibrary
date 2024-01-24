2e0a408893c1973def267c169fa5a89cfa098932
MattR2718 <matthew.ranson2718@gmail.com>
2024-01-24 17:41:27 +0000

    Add project files.

---
+++ VulkanGraphicsLibrary.cpp
@@ -0,0 +1,10 @@
+﻿// VulkanGraphicsLibrary.cpp : Defines the entry point for the application.
+//
+
+#include "VulkanGraphicsLibrary.h"
+
+int main()
+{
+	std::cout << "Hello CMake.\n";
+	return 0;
+}
